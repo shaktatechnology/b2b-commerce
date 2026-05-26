b7950b61465ecce2c471aa71d5673a64e3b5f1bb
Damodar Khanal <deepakkhanal931@gmail.com>
2026-05-26 11:16:19 +0545

    change in code

--- frontend/src/app/products/page.tsx
+++ frontend/src/app/products/page.tsx
@@ -78,6 +78,7 @@ export default async function ProductsListingPage({ searchParams }: PageProps) {
             ))}
           </div>
         )}
+        
       </div>
     </StorefrontLayout>
   );
